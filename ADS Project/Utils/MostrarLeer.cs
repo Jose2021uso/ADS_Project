d5d1f9a48265a76d426d224fcf5ce7bb58cb3dd6
Jose2021uso <85507926+Jose2021uso@users.noreply.github.com>
2022-03-24 22:49:35 -0600

    Agregar archivos de proyecto.

---
+++ ADS Project/Utils/MostrarLeer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ADS_Project.Utils
+{
+    public class MostrarLeer
+    {
+
+        Boolean _Accion;
+
+        public MostrarLeer()
+        {
+
+        }
+
+        public Boolean Accion { get => _Accion; set => _Accion = value; }
+    }
+}
